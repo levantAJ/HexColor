a65b31c70331cf58ae3c63c9e3281e982bc47fde
levantAJ <sirlevantai@gmail.com>
2019-01-27 11:39:14 +0700

    Implements convert hex string to color

---
+++ HexColor/HexColor.h
@@ -0,0 +1,19 @@
+//
+//  HexColor.h
+//  HexColor
+//
+//  Created by ShopBack on 1/27/19.
+//  Copyright © 2019 levantAJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HexColor.
+FOUNDATION_EXPORT double HexColorVersionNumber;
+
+//! Project version string for HexColor.
+FOUNDATION_EXPORT const unsigned char HexColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HexColor/PublicHeader.h>
+
+
